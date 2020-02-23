5d92f0b6581a3ecd96cbe45973b743c38597f111
Shuvamoy Mondal <shuvamoylovesaditi@gmail.com>
2020-02-22 22:40:59 -0800

    Added maven test

--- src/main/scala/KSQL/App.scala
+++ src/main/scala/KSQL/App.scala
@@ -1,9 +1,20 @@
 package KSQL
 
+import org.apache.spark.sql.SparkSession
 /**
  * Hello world!
  *
  */
 object App extends App {
-  println( "Hello World!" )
+
+  val spark = SparkSession.builder
+    .appName("SparkSessionExample")
+    .master("192.168.139.133:6443")
+    .getOrCreate
+
+  import spark.implicits._
+  val emp = Seq((101, "Amy", Some(2)))
+  val employee = spark.createDataFrame(emp).toDF("employeeId","employeeName","managerId")
+  employee.show(1)
+
 }
